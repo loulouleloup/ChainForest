bd9e066b5d0d887ab20a26901dd02c2e999e7098
loulouleloup <louis.plessis@hotmail.fr>
2021-04-13 16:34:51 +0200

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,20 @@
-var Crowdsale = artifacts.require("./Crowdsale.sol");
+var Token = artifacts.require("ERC20Token");
+var ICO = artifacts.require("Ico");
 
-module.exports =  function(deployer) {
- // deployer.deploy(Voting);
-   deployer.deploy(Crowdsale, 'ChainForest', 'CF', '21000000');
+
+module.exports = async function(deployer) {
+  const totalSupply = 21000000;
+  //Token
+  deployer.deploy(Token, totalSupply, 'ChainForest', 'CF');
+  const token = await Token.deployed();
+  console.log('token address ',token.address);
+  
+  //ICO
+   deployer.deploy(ICO, token.address, 3600000, 1, totalSupply, 1, 100);
+   const ico = ICO.deployed();
+
+   await token.updateAdmin(ico.address);
+   await ico.start();
 
   
 };
